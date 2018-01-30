5bed3955409c95772dac7d56aaf4677501dce0f0
Clement Trosa <me@trosa.io>
2018-01-30 09:03:44 +0100

    QuickFix.

---
+++ .shell.nix
@@ -0,0 +1,51 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.shellAliases = {
+    ".." = "cd ..";
+    "..." = "cd ../..";
+    "..2" = "cd ../..";
+    "..3" = "cd ../../..";
+    "..4" = "cd ../../../..";
+  };
+
+  environment.interactiveShellInit = ''
+  	export HISTCONTROL=ignoreboth
+    export HISTSIZE=1000000
+    export HISTFILESIZE=$HISTSIZE
+    shopt -s histappend
+  '';
+
+  environment.profileRelativeEnvVars = {
+    GRC_BLOCKS_PATH = [ "/share/gnuradio/grc/blocks" ];
+    PYTHONPATH = [ "/lib/python2.7/site-packages" ];
+	HOME = [ "/home/iomonad"]
+  };
+
+  environment.sessionVariables = {
+    NIX_AUTO_INSTALL = "1";
+  };
+
+  # Select internationalisation properties.
+  i18n.consoleKeyMap = "qwerty/us";
+
+  programs.bash.enableCompletion = true;
+
+  programs.bash.promptInit = ''
+    export GIT_PS1_SHOWDIRTYSTATE=1
+    source ${pkgs.gitAndTools.gitFull}/share/git/contrib/completion/git-prompt.sh
+    __prompt_color="1;32m"
+    __alternate_color="1;33m"
+    __hostnamecolor="$__prompt_color"
+    # If logged in with ssh, pick a color derived from hostname
+    if [ -n "$SSH_CLIENT" ]; then
+      __hostnamecolor="1;$(${pkgs.nettools}/bin/hostname | od | tr ' ' '\n' | ${pkgs.gawk}/bin/awk '{total = total + $1}END{print 30 + (total % 6)}')m"
+      # Fixup color clash
+      if [ "$__hostnamecolor" = "$__prompt_color" ]; then
+        __hostnamecolor="$__alternate_color"
+      fi
+    fi
+    __red="1;31m"
+    PS1='\n$(ret=$?; test $ret -ne 0 && printf "\[\e[$__red\]$ret\[\e[0m\] ")\[\e[$__prompt_color\]\u@\[\e[$__hostnamecolor\]\h \[\e[$__prompt_color\]\w$(__git_ps1 " [git:%s]")\[\e[0m\]\n$ '
+  '';
+}
